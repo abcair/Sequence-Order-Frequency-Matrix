4479ffa9d21992f8478f45f85da3497321eb9280
work-hard-play-harder <499690505@qq.com>
2017-09-08 08:55:32 +0800

    Update SOFM.py

--- SOFM.py
+++ SOFM.py
@@ -11,16 +11,20 @@ Foundation; either version 2 of the License, or (at your option) any later versi
 This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
 without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 See the GNU General Public License for more details.
- 
+
+Last Motified Time: 2017-09-08
 """
 
-
+import os
 import sys
 import argparse
-
+import subprocess
+import ConfigParser
 import itertools
 import collections
 import numpy as np
+import xml.etree.ElementTree as ET
+from Bio import SeqIO
 
 
 def Kmer2Index(alphabet, kmer):
@@ -31,17 +35,113 @@ def Kmer2Index(alphabet, kmer):
     return kmer2index
 
 
-def readMSA(msa_file):
+def run_search(FASTAfile):
+    # temp file names
+    xml_file = os.path.join(filepath, shortname + '.xml')
+
+    # run psi-blast command
+    outfmt_type = 5
+    num_iter = 3
+    evalue_threshold = 0.001
+    cmd = ' '.join([PSIBLAST,
+                    '-query ' + FASTAfile,
+                    '-db ' + BLAST_DB,
+                    '-out ' + xml_file,
+                    '-evalue ' + str(evalue_threshold),
+                    '-num_iterations ' + str(num_iter),
+                    '-outfmt ' + str(outfmt_type)]
+                   )
+    print cmd
+    return_code = subprocess.call(cmd, shell=True)
+
+    # parser the xml file output by PSIBLAST
+    tree = ET.ElementTree(file=xml_file)
+    query_def = tree.find('BlastOutput_query-def').text
+    query_len = tree.find('BlastOutput_query-len').text
+    # get the last iteration
+    iteration = tree.findall('BlastOutput_iterations/Iteration')[-1]
+    Iteration_hits = iteration.find('Iteration_hits')
+
+    # store searched homology proteins
     MSA = []
-    with open(msa_file) as f:
-        for line in f:
-            MSA.append(line.strip())
+    for Hit in Iteration_hits.getchildren():
+        Hsp_evalue = Hit.find('Hit_hsps/Hsp/Hsp_evalue').text
+
+        # only parser the hits that e-value < threshold
+        if float(Hsp_evalue) > evalue_threshold:
+            continue
+
+        Hit_num = Hit.find('Hit_num').text
+        Hit_id = Hit.find('Hit_id').text
+        Hit_def = Hit.find('Hit_def').text
+
+        Hsp_query_from = Hit.find('Hit_hsps/Hsp/Hsp_query-from').text
+        Hsp_query_to = Hit.find('Hit_hsps/Hsp/Hsp_query-to').text
+        Hsp_hit_from = Hit.find('Hit_hsps/Hsp/Hsp_hit-from').text
+        Hsp_hit_to = Hit.find('Hit_hsps/Hsp/Hsp_hit-to').text
+        Hsp_qseq = Hit.find('Hit_hsps/Hsp/Hsp_qseq').text
+        Hsp_hseq = Hit.find('Hit_hsps/Hsp/Hsp_hseq').text
+
+        # alignment sequence by add prefix, suffix
+        prefix = "-" * (int(Hsp_query_from) - 1)
+        suffix = "-" * (int(query_len) - int(Hsp_query_to))
+
+        # delete the space in protein_name and the corresponding position of
+        # hits
+        pos = -1
+        for aa in Hsp_qseq:
+            pos = pos + 1
+            if aa == '-':
+                Hsp_hseq = Hsp_hseq[:pos] + '*' + Hsp_hseq[pos + 1:]
+        Hsp_hseq = Hsp_hseq.replace('*', '')
+
+        if 'X' in Hsp_hseq:
+            Hsp_hseq = Hsp_hseq.replace('X', '-')
+        if 'B' in Hsp_hseq:
+            Hsp_hseq = Hsp_hseq.replace('B', '-')
+        if 'Z' in Hsp_hseq:
+            Hsp_hseq = Hsp_hseq.replace('Z', '-')
+        if 'U' in Hsp_hseq:
+            Hsp_hseq = Hsp_hseq.replace('U', '-')
+        if 'J' in Hsp_hseq:
+            Hsp_hseq = Hsp_hseq.replace('J', '-')
+        if 'O' in Hsp_hseq:
+            Hsp_hseq = Hsp_hseq.replace('O', '-')
+
+        # combine prefix, modified hits, suffix
+        hit_sequence = prefix + Hsp_hseq + suffix
+        # print hit_sequence
+
+        # append in MSA
+        MSA.append(hit_sequence)
+
+    if MSA == []:
+        # append the protein-self
+        ff = open(fasta_file, 'r')
+        ff.readline()  # skip the id
+        fasta_seq = ff.readline().strip().upper()
+        ff.close()
+
+        if 'X' in fasta_seq:
+            fasta_seq = fasta_seq.replace('X', '-')
+        if 'B' in fasta_seq:
+            fasta_seq = fasta_seq.replace('B', '-')
+        if 'Z' in fasta_seq:
+            fasta_seq = fasta_seq.replace('Z', '-')
+        if 'U' in fasta_seq:
+            fasta_seq = fasta_seq.replace('U', '-')
+        if 'J' in fasta_seq:
+            fasta_seq = fasta_seq.replace('J', '-')
+        if 'O' in fasta_seq:
+            fasta_seq = fasta_seq.replace('O', '-')
+
+        MSA.append(fasta_seq)
 
     return MSA
 
 
 def GenerateSOFM(MSA, kmer, kmer2index):
-    # initialize SOFM    
+    # initialize SOFM
     row_size = len(kmer2index)
     column_size = len(MSA[0]) - kmer + 1
     SOFM = np.zeros((row_size, column_size))
@@ -63,7 +163,7 @@ def GenerateSOFM(MSA, kmer, kmer2index):
     return SOFM.transpose()
 
 
-def WriteSOFM(SOFMfile, SOFM, kmer2index):
+def WriteSOFM(FASTAseq, SOFMfile, SOFM, kmer2index):
     headers = sorted(kmer2index.iteritems(), key=lambda d: d[1])
 
     with open(SOFMfile, 'w') as f:
@@ -73,37 +173,54 @@ def WriteSOFM(SOFMfile, SOFM, kmer2index):
         f.write('\n')
 
         for i in xrange(0, SOFM.shape[0]):
-            f.write("{:<9s}".format(str(i + 1) + '   '))  # rower number
+            # rower number and fasta sequence
+            f.write("{:<4s}".format(str(i + 1))+"{:<5s}".format(FASTAseq[i]))
             for j in xrange(0, SOFM.shape[1]):
                 f.write("{:<8s}".format(str(SOFM[i, j])))
             f.write('\n')
 
 
-def main(MSAfile, SOFMfile, kmer, kmer2index):
-    MSA = readMSA(MSAfile)
+def main(FASTAfile, SOFMfile, kmer):
+    alphabet = 'ACDEFGHIKLMNPQRSTVWY'
+    # a dictionary that contains all amino acid substrings with length k
+    kmer2index = Kmer2Index(alphabet, kmer)
+
+    MSA = run_search(FASTAfile)
     if MSA != []:
         SOFM = GenerateSOFM(MSA, kmer, kmer2index)
 
-    WriteSOFM(SOFMfile, SOFM, kmer2index)
+    # extract fasta sequence
+    FASTAseq = list(SeqIO.parse(open(FASTAfile), 'fasta'))[0].seq.upper()
+    WriteSOFM(FASTAseq, SOFMfile, SOFM, kmer2index)
 
 
 if __name__ == '__main__':
     # parameters
     parser = argparse.ArgumentParser(description="program description")
-    parser.add_argument('MSAfile', help='input MSA file')
-    parser.add_argument('SOFMfile', help='output SOFM file')
-    parser.add_argument('-k', '--kmer', type=int, default=3,
+    parser.add_argument('-i', '--input', required=True,
+                        help='input a single protein sequence in FASTA format')
+    parser.add_argument('-o', '--output', default=None, required=False,
+                        help='output SOFM file')
+    parser.add_argument('-k', '--kmer', type=int, default=3, required=False,
                         help='length of substrings')
     #parser.add_argument('-norm', type=bool, default=True, help='normlize the column')
     args = parser.parse_args()
 
-    MSAfile = args.MSAfile
-    SOFMfile = args.SOFMfile
+    FASTAfile = args.input
+    SOFMfile = args.output
     kmer = args.kmer
-    # print MSAfile, SOFMfile, kmer
 
-    alphabet = 'ACDEFGHIKLMNPQRSTVWY'
-    # a dictionary that contains all amino acid substrings with length k
-    kmer2index = Kmer2Index(alphabet, kmer)
+    if not SOFMfile:
+        (filepath, filename) = os.path.split(FASTAfile)
+        (shortname, extension) = os.path.splitext(filename)
+        SOFMfile = os.path.join(filepath, shortname + '.sofm' + str(kmer))
+    print FASTAfile, SOFMfile, kmer
 
-    main(MSAfile, SOFMfile, kmer, kmer2index)
+    conf = ConfigParser.ConfigParser()
+    conf.read('PATH.conf')
+    PSIBLAST = os.path.join(conf.get('PSIBLAST', 'NCBI_BLAST_BIN'),
+                            'psiblast')
+    BLAST_DB = conf.get('PSIBLAST', 'BLAST_DATABASE')
+    print PSIBLAST, BLAST_DB
+
+    main(FASTAfile, SOFMfile, kmer)
